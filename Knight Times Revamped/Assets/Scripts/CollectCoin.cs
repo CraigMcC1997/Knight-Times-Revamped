f2ddf21a3fe1a8cb14c38ceb835496116dc67eaf
Craig McCorrisken <craigmccorrisken@gmail.com>
2022-08-23 18:57:58 +0100

    renamed coin collection script
    Signed-off-by: Craig McCorrisken <craigmccorrisken@gmail.com>

--- Knight Times Revamped/Assets/Scripts/CollectCoin.cs
+++ Knight Times Revamped/Assets/Scripts/CollectCoin.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeleteCoin : MonoBehaviour
+public class CollectCoin : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
